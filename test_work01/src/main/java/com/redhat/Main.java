288ec12ceb76ce4a23299ac92a072f755cf0faa4
obabec <ond.babec@gmail.com>
2017-11-28 15:07:42 +0100

    Basic func from train

--- test_work01/src/main/java/com/redhat/Main.java
+++ test_work01/src/main/java/com/redhat/Main.java
@@ -1,4 +1,46 @@
 package com.redhat;
 
-public class Main {
+import javax.swing.text.html.parser.Parser;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main
+{
+    public static List<Integer> numbersToSort = new ArrayList<>();
+
+
+    public static void main(String args[])
+    {
+        Integer[] a = new Integer[args.length];
+
+        for (int i = 0; i < args.length; i++)
+        {
+            a[i] = Integer.parseInt(args[i]);
+        }
+
+      numbersToSort = Arrays.asList(a);
+        bubble();
+    }
+
+
+
+    public static void bubble()
+    {
+        int transfer = 0;
+        for(int x = 0;x<numbersToSort.size();x++)
+        {
+            for(int y=0;y<numbersToSort.size();y++)
+            {
+                    if (numbersToSort.get(x)>numbersToSort.get(y))
+                    {
+                        transfer = numbersToSort.get(x);
+                        numbersToSort.set(x, numbersToSort.get(y));
+                        numbersToSort.set(y, transfer);
+                    }
+            }
+        }
+
+        numbersToSort.forEach(System.out::println);
+    }
 }
