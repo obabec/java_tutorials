b088a8516945ff154e30990e7d8e1830e97d7afa
obabec <ond.babec@gmail.com>
2017-12-12 12:43:04 +0100

    Maybe it' s right

--- test_work01/src/main/java/com/redhat/BubbleSorter.java
+++ test_work01/src/main/java/com/redhat/BubbleSorter.java
@@ -5,26 +5,25 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class BubbleSorter implements BasicSorter {
+public class BubbleSorter <T extends Comparable<T>> implements BasicSorter {
+
+    public List<T> mySortedList;
 
     @Override
     public List sort(Collection collection)
     {
-        List<? extends Comparable> mySortedList;
-
-        mySortedList = (List<? extends Comparable>) collection.stream().collect(Collectors.toList());
-
-        Comparable transfer;
+        mySortedList = (List) collection.stream().collect(Collectors.toList());
+        T transfer;
         for(int x = 0;x<mySortedList.size()-1;x++)
         {
 
             for(int y=1+x;y<mySortedList.size();y++)
             {
-                if (1 == mySortedList.get(x).compareTo(mySortedList.get(y)))
+                if (1 == mySortedList.get(y).compareTo(mySortedList.get(x)))
                 {
-                    transfer = (Comparable) mySortedList.get(x);
+                    transfer =  mySortedList.get(x);
                     mySortedList.set(x, mySortedList.get(y));
-                    mySortedList.set(y, );
+                    mySortedList.set(y, transfer);
 
                 }
             }
